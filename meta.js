bdaaff2746bacee726b17cdd622b27159fffb170
idontgiveaf <156725392+1dontgiveaf@users.noreply.github.com>
2024-12-07 19:13:41 -0500

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Simple Arceus Bypass
+// @namespace    https://discord.gg/5vNgzbUsA3
+// @version      2.0.0
+// @description  Instant bypass for Arceus X.
+// @author       @.idontgiveaf on discord
+// @run-at       document-end
+
+// @match        *://*.linkvertise.com/*
+// @match        *://*.linkvertise.net/*
+// @match        *://*.link-to.net/*
+
+
+// @exclude      *://publisher.linkvertise.com/*
+// @exclude      *://linkvertise.com/search*
+// @exclude      *://linkvertise.com/login*
+// @exclude      *://linkvertise.com/profile*
+// @exclude      *://blog.linkvertise.com
+// @exclude      *://blog.linkvertise.com/*
+// @exclude      *://linkvertise.com/assets/vendor/*
+// @exclude      *://publisher.linkvertise.com/*
+// @exclude      *://link-mutation.linkvertise.com/*
+// @exclude      *://linkvertise.com/assets/external/thinksuggest
+
+
+// @match        https://loot-link.com/s?*
+// @match        https://loot-links.com/s?*
+// @match        https://lootlink.org/s?*
+// @match        https://lootlinks.co/s?*
+// @match        https://lootdest.info/s?*
+// @match        https://lootdest.org/s?*
+// @match        https://lootdest.com/s?*
+// @match        https://links-loot.com/s?*
+// @match        https://linksloot.net/s?*
+
+
+// @match        https://spdmteam.com/key-system*
+
+
+// @grant        none
+// @license      Creative Commons License 4.0 ((https://creativecommons.org/licenses/by-nc/4.0/)
+// @downloadURL  https://github.com/1dontgiveaf/userscripts/raw/refs/heads/main/user.js
+// @updateURL    https://github.com/1dontgiveaf/userscripts/raw/refs/heads/main/meta.js
+// ==/UserScript==
